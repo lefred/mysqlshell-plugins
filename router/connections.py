02ff7ab8032c9a9736a9f46fdaee2a6495302b46
Frederic Descamps <lefred.descamps@gmail.com>
2019-09-17 19:29:02 +0200

    fix output (but double the connections)
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- router/connections.py
+++ router/connections.py
@@ -31,9 +31,33 @@ def __cluster_routes(router_ip, router_port, user, password, route_to_find):
     result = __router_call("/routes", router_ip, router_port, user, password)
     if result: 
         result_json = json.loads(result.content)
-        fmt = "| {0:22s} | {1:15s} | {2:12s} | {3:>20s} | {4:>20s} | {5:27s} |"
+        
+        route_size = 22
+        source_size = 15
+        destination_size = 12
+        from_size = 20
+        to_size = 20
+        
+        for item in result_json['items']:
+           if len(item['name']) > route_size:
+              route_size = len(item['name'])
+              if route_to_find in route_name:
+                    result_item = __router_call("/routes/%s/connections" % route_name, router_ip, router_port, user, password)
+                    result_item_json = json.loads(result_item.content)
+                    if len(result_item_json['items']) > 0:
+                        for entry in result_item_json['items']:
+                           if len(entry['sourceAddress']) > source_size:
+                              source_size =  entry['sourceAddress']
+                           if len(entry['destinationAddress']) > destination_size:
+                              destination_size = entry['destinationAddress']
+                           if len(entry['bytesFromServer']) > from_size:
+                              from_size = len(entry['bytesFromServer'])
+                           if len(entry['bytesToServer']) > to_size:
+                              to_size = len(entry['bytesToServer'])
+            
+        fmt = "| {0:%ds} | {1:%ds} | {2:%ds} | {3:>%ds} | {4:>%ds} | {5:27s} |" % (route_size, source_size, destination_size, from_size)
         header = fmt.format("Route", "Source", "Destination", "From Server", "To Server", "Connection Started")
-        bar = "+" + "-" * 24 + "+" + "-" * 17 + "+" + "-" * 14 + "+" + "-" * 22 + "+" + "-" * 22 + "+" + "-" * 29 + "+"
+        bar = "+" + "-" * (route_size + 2) + "+" + "-" * (source_size + 2) + "+" + "-" * (destination_size + 2) + "+" + "-" * (from_size + 2) + "+" + "-" * (to_size + 2) + "+" + "-" * 29 + "+"
         print (bar)
         print (header)
         print (bar)
