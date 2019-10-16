12df67989feea07dcd25c9d718f3423ae259d23d
Frederic Descamps <lefred.descamps@gmail.com>
2019-10-16 19:40:32 +0200

    - update trx size from binlogs - get binlog list
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- audit/init.py
+++ audit/init.py
@@ -3,7 +3,32 @@ from ext.audit import trx
 
 
 register_plugin("showTrxSize", trx.show_trx_size,
-           {"brief": "Prints Transactions Size from last binlog",
+           {"brief": "Prints Transactions Size from a binlog",
+             "parameters": [{
+                "name": "binlog",
+                "brief": "The binlog file to extract transactions from.",
+                "type": "string",
+                "required": False
+            },{
+                "name": "session",
+                "brief": "The session to be used on the operation.",
+                "type": "object",
+                "classes": ["Session", "ClassicSession"],
+                "required": False
+            }
+            ]
+           },
+           "audit",
+           {
+             "brief": "Audit management and utilities.",
+             "details": [
+                 "A collection of Audit management tools and utilities"
+             ]
+           }
+     )
+
+register_plugin("getBinlogs", trx.show_binlogs,
+           {"brief": "Prints the list of binary logs available on the server",
              "parameters": [{
                 "name": "session",
                 "brief": "The session to be used on the operation.",
--- audit/trx.py
+++ audit/trx.py
@@ -3,9 +3,10 @@
 # Definition of member functions for the audit extension object to display trx info
 #
 
-def __returnBinlogEvents(session):
-    stmt = "SHOW BINLOG EVENTS"
-    result = session.run_sql(stmt)
+def _returnBinlogEvents(session, binlog):
+    stmt = "SHOW BINLOG EVENTS in '%s'" % binlog
+    print(stmt)
+    result = session.run_sql(stmt) 
     events = result.fetch_all()
     if (result.get_warnings_count() > 0):
         # Bail out and print the warnings
@@ -15,7 +16,24 @@ def __returnBinlogEvents(session):
 
     return events
 
-def __format_bytes(size):
+def _returnBinlogs(session):
+    stmt = "SHOW BINARY LOGS"
+    result = session.run_sql(stmt)
+    binlogs = result.fetch_all()
+    if (result.get_warnings_count() > 0):
+        # Bail out and print the warnings
+        print("Warnings occurred - bailing out:")
+        print(result.get_warnings())
+        return False
+    if len(binlogs) == 0:
+        print("No binary log files present")
+        return False
+
+    return binlogs
+
+
+
+def _format_bytes(size):
     # 2**10 = 1024
     power = 2**10
     for unit in ('bytes', 'kb', 'mb', 'gb'):
@@ -25,8 +43,26 @@ def __format_bytes(size):
 
     return "%d tb" % (size,)
 
+def show_binlogs(session=None):
+    # Get hold of the global shell object
+    import mysqlsh
+    shell = mysqlsh.globals.shell
 
-def show_trx_size(session=None):
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                  "function or connect the shell to a database")
+            return
+    binlogs = _returnBinlogs(session)
+    if binlogs:
+       print("Binary log file(s) present:")
+       for entry in binlogs:
+          print(entry[0]) 
+ 
+    return  
+
+def show_trx_size(binlog=None, session=None):
     # Get hold of the global shell object
     import mysqlsh
     shell = mysqlsh.globals.shell
@@ -38,11 +74,24 @@ def show_trx_size(session=None):
                   "function or connect the shell to a database")
             return
 
-    binlog_events = __returnBinlogEvents(session)
+    binlogs = _returnBinlogs(session)
+    binlog_files = []
+    if binlogs:
+       for entry in binlogs:
+          binlog_files.append(entry[0]) 
+
+    if binlog is None:
+        binlog = binlog_files[-1]
+    else:
+        if binlog not in binlog_files:
+           print("%s not present on the server" % binlog)
+           return
+    
+    binlog_events = _returnBinlogEvents(session, binlog)
     print_binlog = True
     for row in binlog_events:
        if print_binlog:
           print("Transactions in binary log %s:" % row[0])
           print_binlog = False
-       print("%s" % __format_bytes(row[4]-row[1]))
+       print("%s" % _format_bytes(row[4]-row[1]))
     return 
