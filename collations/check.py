aa4ee1bd272696c677877fd46bd8a03ad50c1ef6
Frederic Descamps <lefred.descamps@gmail.com>
2019-10-18 13:29:27 +0200

    - add check for collation (existing or not) - add total of records
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- collations/check.py
+++ collations/check.py
@@ -6,6 +6,15 @@
 # KEY or have a unique constraint e.g when migarting from latin1 to
 # utf8mb4.
 
+def _check_if_collation_exists(session, collation):
+   query = """SELECT COLLATION_NAME FROM INFORMATION_SCHEMA.COLLATIONS 
+              where COLLATION_NAME = '%s'""" % collation
+   result = session.run_sql(query)
+   if len(result.fetch_all()) == 1:
+       return True
+   return False
+    
+
 def non_unique(table, col, collation, schema=None, session=None):
     # Get hold of the global shell object
     import mysqlsh
@@ -23,9 +32,15 @@ def non_unique(table, col, collation, schema=None, session=None):
             print("No schema specified. Either pass a schema name or use one")
             return
         schema = session.current_schema.name
+    if not _check_if_collation_exists(session, collation):
+        print("ERROR: %s is not a valid collation !" % collation)
+        return
+    query = "select count(*) from %s.%s" % (schema, table)
+    result = session.run_sql(query)
+    tot = result.fetch_one()[0]
 
     charset = collation.split("_")[0];
-    
+
     query = "select "+col+" as problematic from (select "+col+", count("+col+") over (partition by convert("+col+" using "+charset+") collate "+collation+") as cnt from "+table+") as t  where cnt > 1;"
 
     result = session.run_sql(query)
@@ -36,4 +51,4 @@ def non_unique(table, col, collation, schema=None, session=None):
     for row in offending:
         print(row[0])
     print("=================")
-    return "Total: %d non-unique values" % len(offending)
+    return "Total: %d non-unique values out of %d" % (len(offending), tot)
