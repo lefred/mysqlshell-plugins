e6223b0f404967b83b3bac2618df3ec676c4ad7d
Bernt M. Johnsen <bernt.johnsen@oracle.com>
2019-10-18 19:00:08 +0200

    Outoforder: Check if a column chnages order with chnage of collation

--- collations/init.py
+++ collations/init.py
@@ -4,11 +4,52 @@
 
 from ext.mysqlsh_plugins_common import register_plugin
 from ext.collations import check as check_src 
+from ext.collations import outoforder as outoforder_src 
 
 
 register_plugin("nonUnique", check_src.non_unique,
                 {
                     "brief": "Find non-unique values in a column for a given collation",
+                    "parameters": [
+                        {
+                        "name": "table",
+                        "brief": "Table name to use.",
+                        "type": "string",
+                        "required": True},
+                        {
+                        "name": "column",
+                        "brief": "column to check.",
+                        "type": "string",
+                        "required": True},
+                        {
+                        "name": "collation",
+                        "brief": "collation to check.",
+                        "type": "string",
+                            "required": True},
+                        {
+                        "name": "schema",
+                        "brief": "Schema to use.",
+                        "type": "string",
+                            "required": False},
+                        {
+                        "name": "session",
+                        "brief": "The session to be used on the operation.",
+                        "type": "object",
+                        "classes": ["Session", "ClassicSession"],
+                        "required":False}
+                    ]
+                },
+                "collations",
+                {
+                    "brief": "Collation utilities.",
+                    "details": [
+                        "A collection of collation utilites"
+                    ]
+                })
+
+register_plugin("outOfOrder", outoforder_src.out_of_order,
+                {
+                    "brief": "Find values in a column that becomes out of order for a given collation",
                     "parameters": [
                         {
                         "name": "table",
