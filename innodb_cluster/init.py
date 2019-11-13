f8e4426b65995e5bbd12f376c243330ab3e61c2d
Frederic Descamps <lefred.descamps@gmail.com>
2019-11-13 09:39:04 +0100

    add innodb cluster replication transaction speed
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

---
+++ innodb_cluster/init.py
@@ -0,0 +1,29 @@
+from ext.mysqlsh_plugins_common import register_plugin
+from ext.innodb_cluster import secondary
+
+
+register_plugin("showGroupReplicationSpeed", secondary.show_speed,
+           {"brief": "Prints Transactions Size from a binlog",
+             "parameters": [{
+                "name": "limit",
+                "brief": "For how many seconds the output show be displayed, refreshed every second and default is 10",
+                "type": "integer",
+                "required": False
+              },{
+                "name": "session",
+                "brief": "The session to be used on the operation. This must be a session to a InnoB Cluster member",
+                "type": "object",
+                "classes": ["Session", "ClassicSession"],
+                "required": False
+            }
+            ]
+           },
+           "innodb_cluster",
+           {
+             "brief": "MySQL InnoDB Cluster management and utilities.",
+             "details": [
+                 "A collection of MySQL InnoDB Cluster management tools and utilities"
+             ]
+           }
+     )
+
---
+++ innodb_cluster/secondary.py
@@ -0,0 +1,115 @@
+from mysqlsh import mysql
+import time
+
+def _get_secondary(session):
+       stmt = """select concat(member_host,':',member_port) `member` 
+                 from performance_schema.replication_group_members 
+                 where member_role='SECONDARY'"""
+       result = session.run_sql(stmt)
+       secondaries = []
+       rows = result.fetch_all()
+       for row in rows:
+         secondaries.append(row[0])
+       secondaries.sort() 
+       return secondaries
+
+def _get_members(session):
+       stmt = """select concat(member_host,':',member_port) `member` 
+                 from performance_schema.replication_group_members"""
+       result = session.run_sql(stmt)
+       members = []
+       rows = result.fetch_all()
+       for row in rows:
+         members.append(row[0])
+       members.sort() 
+       return members
+
+def _check_session_cred(shell, uri):
+    tofind = uri.split('/')[2]
+    print(uri)
+    if tofind not in shell.list_credentials():
+        print("%s is not stored in your Shell Credentials")
+        answer = shell.prompt('Do you want to connect to it now ? (Y/n) ', {'defaultValue':'y'})
+        if answer.lower() == 'y':
+            session_sec = shell.connect(uri)
+            session_sec.close()
+    return
+
+def _connect_to_secondary(shell, session, secondary):
+    uri_json = shell.parse_uri(session.get_uri())
+    uri_json['host'] = secondary.split(':')[0]
+    uri_json['port'] = secondary.split(':')[1] 
+    uri_json['scheme'] = "mysql"
+    uri = shell.unparse_uri(uri_json)
+    
+    #print("Using %s to connect to secondary..." % uri)
+    #_check_session_cred(shell, uri)
+    password = shell.prompt('Please enter password for %s: ' % uri, {'type': 'password'})
+    session2 = mysql.get_session(uri, password)
+    return session2
+
+def _get_cluster_mode(session, dba):
+   return dba.get_cluster().status()['defaultReplicaSet']['topologyMode']
+
+def show_speed(limit=10,session=None):
+
+    import mysqlsh
+    shell = mysqlsh.globals.shell
+    secondary_sessions = {}
+
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                  "function or connect the shell to a database")
+            return
+    cluster_mode = _get_cluster_mode(session, mysqlsh.globals.dba)
+   
+
+    if cluster_mode == 'Single-Primary':
+       secondaries = _get_secondary(session)
+    else: 
+       secondaries = _get_members(session)
+
+    if len(secondaries) == 0:
+        print("No Secondary Members discovered !")
+        return
+    out = ""
+    head0 = ""
+    head1 = ""
+    head2 = ""
+    for secondary in secondaries:
+        secondary_sessions[secondary] = _connect_to_secondary(shell, session, secondary)
+    head0 = "		SeqNo				SeqNo							"
+    head1 = "		Last				Last		transport	time to				"
+    head2 = "Host		ApplyTx		repl delay	QueueTx		time		RelayLog	apply time	"
+    print("=" * 106)
+    print("MySQL InnoDB Cluster : Group Replication Information")
+    print("=" * 106)
+    print(head0)
+    print(head1)
+    print(head2)
+    print("-" * 106)
+    
+    for i in range(limit):
+            out = ""
+            for secondary in secondaries:
+               stmt = """SELECT LAST_APPLIED_TRANSACTION, LAST_QUEUED_TRANSACTION, 
+                         LAST_APPLIED_TRANSACTION_END_APPLY_TIMESTAMP - 
+	                 LAST_APPLIED_TRANSACTION_ORIGINAL_COMMIT_TIMESTAMP 'rep delay (sec)', 
+                         LAST_QUEUED_TRANSACTION_START_QUEUE_TIMESTAMP - 
+                            LAST_QUEUED_TRANSACTION_ORIGINAL_COMMIT_TIMESTAMP 'transport time', 
+                         LAST_QUEUED_TRANSACTION_END_QUEUE_TIMESTAMP - 
+                            LAST_QUEUED_TRANSACTION_START_QUEUE_TIMESTAMP 'time RL',
+                         LAST_APPLIED_TRANSACTION_END_APPLY_TIMESTAMP - 
+                            LAST_APPLIED_TRANSACTION_START_APPLY_TIMESTAMP 'apply time'
+                         FROM performance_schema.replication_applier_status_by_worker t1 
+                         JOIN performance_schema.replication_connection_status t2 
+                           ON t2.channel_name=t1.channel_name  
+                        WHERE t1.channel_name='group_replication_applier'"""
+               result = secondary_sessions[secondary].run_sql(stmt)
+               row = result.fetch_one()
+               out = "%s	%s		%s	%s		%s	%s	%s	" % (secondary, row[0].split(':')[1], row[2], row[1].split(':')[1], row[3], row[4], row[5]) 
+               print(out) 
+            print(" ")
+            time.sleep(1)
