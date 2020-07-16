0673e3b18f7bd9f61d410a2b7e2ac764055bcd73
Frederic Descamps <lefred.descamps@gmail.com>
2020-07-16 15:31:54 +0200

    add auto_increment fill information
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

---
+++ innodb/autoinc.py
@@ -0,0 +1,77 @@
+from ext.mysqlsh_plugins_common import run_and_show
+
+def get_autoinc_fill(percentage=50, schema=None, session=None):
+    
+    where_filter = ""
+    having_filter = ""
+
+    import mysqlsh
+    shell = mysqlsh.globals.shell
+
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                "function or connect the shell to a database")
+            return
+    stmt = "select @@information_schema_stats_expiry;"
+    result = session.run_sql(stmt)
+    stats = result.fetch_all()
+    if len(stats) > 0:
+       for stat in stats:
+           if int(stat[0]) > 0:
+               print ("Warning: information_schema_stats_expiry is set to {0}.".format(*stat))
+               answer = shell.prompt("""Do you want to change it ? (y/N) """
+                              , {'defaultValue':'n'})
+               if answer.lower() == 'y':
+                    stmt = """SET information_schema_stats_expiry=0"""
+                    result = session.run_sql(stmt)
+
+    if percentage > 0:
+        having_filter = "HAVING CAST(AUTO_INCREMENT_RATIO AS SIGNED INTEGER) >= {}".format(percentage)
+    if schema:
+        where_filter = "{} AND TABLE_SCHEMA='{}'".format(where_filter, schema)
+
+    stmt = """SELECT
+        TABLE_SCHEMA,
+        TABLE_NAME,
+        COLUMN_NAME,
+        DATA_TYPE,
+        COLUMN_TYPE,
+        IF(
+        LOCATE('unsigned', COLUMN_TYPE) > 0,
+        1,
+        0
+        ) AS IS_UNSIGNED,
+        (
+        CASE DATA_TYPE
+        WHEN 'tinyint' THEN 255
+        WHEN 'smallint' THEN 65535
+        WHEN 'mediumint' THEN 16777215
+        WHEN 'int' THEN 4294967295
+        WHEN 'bigint' THEN 18446744073709551615
+        END >> IF(LOCATE('unsigned', COLUMN_TYPE) > 0, 0, 1)
+        ) AS MAX_VALUE,
+        AUTO_INCREMENT, CONCAT(ROUND(
+        AUTO_INCREMENT / (
+        CASE DATA_TYPE
+        WHEN 'tinyint' THEN 255
+        WHEN 'smallint' THEN 65535
+        WHEN 'mediumint' THEN 16777215
+        WHEN 'int' THEN 4294967295
+        WHEN 'bigint' THEN 18446744073709551615
+        END >> IF(LOCATE('unsigned', COLUMN_TYPE) > 0, 0, 1)
+        )*100), '%') AS AUTO_INCREMENT_RATIO
+        FROM
+        INFORMATION_SCHEMA.COLUMNS
+        INNER JOIN INFORMATION_SCHEMA.TABLES USING (TABLE_SCHEMA, TABLE_NAME)
+        WHERE
+        TABLE_SCHEMA NOT IN ('mysql', 'INFORMATION_SCHEMA', 'performance_schema')
+        AND EXTRA='auto_increment' {}
+        {}
+        ORDER BY CAST(AUTO_INCREMENT_RATIO AS SIGNED INTEGER) 
+        """.format(where_filter, having_filter)
+    
+    run_and_show(stmt)
+
+    return
--- innodb/init.py
+++ innodb/init.py
@@ -6,6 +6,7 @@ from ext.mysqlsh_plugins_common import register_plugin
 from ext.innodb import fragmented
 from ext.innodb import progress
 from ext.innodb import bufferpool
+from ext.innodb import autoinc
 
 
 register_plugin("getFragmentedTables", fragmented.get_fragmented_tables,
@@ -75,6 +76,30 @@ register_plugin("getAlterProgress", progress.get_alter_progress,
 register_plugin("getTablesInBP", bufferpool.get_tables_in_bp,
            {"brief": "Prints Tables in BP with some statistics",
              "parameters": [{
+                "name": "session",
+                "brief": "The session to be used on the operation.",
+                "type": "object",
+                "classes": ["Session", "ClassicSession"],
+                "required": False
+            }
+            ]
+           },
+           "innodb"
+     )
+
+register_plugin("getAutoincFill", autoinc.get_autoinc_fill,
+           {"brief": "Prints information about auto_increment fill up",
+             "parameters": [{
+                "name": "percentage",
+                "brief": "Only shows the tables where auto increments values are filled to at least % (default is 50)",
+                "type": "integer",
+                "required": False
+             },{
+                "name": "schema",
+                "brief": "Perform the check only in that specific schema",
+                "type": "string",
+                "required": False
+             },{
                 "name": "session",
                 "brief": "The session to be used on the operation.",
                 "type": "object",
