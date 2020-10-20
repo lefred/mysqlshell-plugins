d8d24c06d6827231d5074be2a69d031e52fdf44f
Miguel Araujo <miguel.araujo@oracle.com>
2020-10-20 17:38:01 +0100

    Fixed Router plugin to work with MySQL Shell 8.0.22

--- router/init.py
+++ router/init.py
@@ -4,32 +4,29 @@
 from router import status as router_status
 from router.myrouter import MyRouter
 
+from mysqlsh.plugin_manager import plugin, plugin_function
+@plugin
+class router:
+    """
+    MySQL Router Object.
 
-router = shell.create_extension_object()
+    MySQL Router Object.
+    """
 
-
-global_obj = shell.create_extension_object()
-
-
+@plugin_function("router.create")
 def create(uri):
+    """
+    Create the MySQL Router Object.
+
+    Args:
+        uri (string): Connection uri to Router's HTTP interface.
+
+    Returns:
+        The newly created Router object
+    """
     my_router = MyRouter(uri)
     return {
          'connections': lambda route_to_find="": my_router.connections(route_to_find),
          'status': lambda: my_router.status(),
          'api': my_router.api
     }
-
-shell.add_extension_object_member(router, 'create', lambda uri=False:create(uri),
-            {
-                'brief':'Create the MySQL Router Object', 'details':['It has MyRouter methods.'],
-                'parameters':[
-                                {'name':'uri', 'type':'string', 'required':True, 'brief':'connection uri to ProxySQL Admin port'},
-                        ]
-                }
-            )
-
-shell.add_extension_object_member(global_obj, 'router', router,
-{
-    'brief':'MySQL Router Object',
-    'details':['MySQL Router Object.']
-})
--- router/myrouter.py
+++ router/myrouter.py
@@ -19,12 +19,12 @@ class MyRouter:
         self.uri = uri
         self.user = shell.parse_uri(self.uri)['user']
         self.ip = shell.parse_uri(self.uri)['host']
-        self.port = shell.parse_uri(self.uri)['port'] 
+        self.port = shell.parse_uri(self.uri)['port']
         if not "password" in shell.parse_uri(self.uri):
             self.__password = shell.prompt('Password: ',{'type': 'password'})
         else:
             self.__password = shell.parse_uri(self.uri)['password']
-   
+
     def __format_bytes(self, size):
         # 2**10 = 1024
         power = 2**10
@@ -56,7 +56,7 @@ class MyRouter:
 
     def __cluster_routes(self, route_to_find):
         result = self.__router_call("/routes")
-        if result: 
+        if result:
             result_json = json.loads(result.content)
             fmt = "| {0:22s} | {1:22s} | {2:12s} | {3:>20s} | {4:>20s} | {5:27s} |"
             header = fmt.format("Route", "Source", "Destination", "From Server", "To Server", "Connection Started")
@@ -71,19 +71,19 @@ class MyRouter:
                         result_item_json = json.loads(result_item.content)
                         if len(result_item_json['items']) > 0:
                             for entry in result_item_json['items']:
-                                print (fmt.format(route_name, entry['sourceAddress'], entry['destinationAddress'], 
-                                str(self.__format_bytes(entry['bytesFromServer'])), 
+                                print (fmt.format(route_name, entry['sourceAddress'], entry['destinationAddress'],
+                                str(self.__format_bytes(entry['bytesFromServer'])),
                                 str(self.__format_bytes(entry['bytesToServer'])), entry['timeStarted']))
                                 route_name=""
                         else:
                                 print (fmt.format(route_name, " "," ", " ", " ",  " "))
             print (bar)
 
-                        
+
 
     def __cluster_metadata_status(self, cluster_name):
         result = self.__router_call("/metadata/%s/status" % cluster_name)
-        if result:    
+        if result:
             result_json = json.loads(result.content)
             print ("     Refresh Succeeded: " + str(result_json['refreshSucceeded']))
             print ("        Refresh Failed: " + str(result_json['refreshFailed']))
@@ -92,7 +92,7 @@ class MyRouter:
 
     def __cluster_all_routes(self):
         result = self.__router_call("/routes")
-        if result:    
+        if result:
             print ("   +--------+")
             print ("   | routes |")
             print ("   +--------+")
@@ -100,16 +100,16 @@ class MyRouter:
             for item in result_json['items']:
                     route_name = item['name']
                     result_item = self.__router_call("/routes/%s/health" % route_name)
-                    result_item_json = json.loads(result_item.content) 
+                    result_item_json = json.loads(result_item.content)
                     if result_item_json['isAlive']:
                             print("    * %s (alive) :" % route_name)
                     else:
                             print("    * %s (dead)  :" % route_name)
 
                     result_status = self.__router_call("/routes/%s/status" % route_name)
-                    result_status_json= json.loads(result_status.content) 
-                    print("\tTotal Connections: %d\tActive Connections: %d\tBlocked Hosts: %d" 
-                                    % (result_status_json['totalConnections'], result_status_json['activeConnections'], 
+                    result_status_json= json.loads(result_status.content)
+                    print("\tTotal Connections: %d\tActive Connections: %d\tBlocked Hosts: %d"
+                                    % (result_status_json['totalConnections'], result_status_json['activeConnections'],
                                     result_status_json['blockedHosts']))
                     result_routes = self.__router_call("/routes/%s/destinations" % route_name)
                     result_routes_json = json.loads(result_routes.content)
@@ -118,7 +118,7 @@ class MyRouter:
 
     def __cluster_name(self):
         result = self.__router_call("/metadata")
-        if result:    
+        if result:
             result_json = json.loads(result.content)
             cluster_name = result_json['items'][0]['name']
             print ("+" + "-" * 16 + "-" * len(cluster_name) + "+")
@@ -133,12 +133,9 @@ class MyRouter:
         if cluster_name:
                 self.__cluster_metadata_status(cluster_name)
                 self.__cluster_all_routes()
-        
+
 
     def connections(self, route_to_find=""):
         self.__cluster_routes(route_to_find)
 
     api = "20190715"
-
-
-       
