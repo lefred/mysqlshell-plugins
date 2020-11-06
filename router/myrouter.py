94676b360e7925569d395e2d7faf8dd699f2a599
Frederic Descamps <lefred.descamps@gmail.com>
2020-11-06 09:52:25 +0100

    oups ;)
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- router/myrouter.py
+++ router/myrouter.py
@@ -136,7 +136,7 @@ class MyRouter:
                     else:
                             print("    * %s (dead)  :" % route_name)
                     result_config = self.__router_call("/routes/%s/config" % route_name)
-                    print("\tRouting Strategy: {}\tMode: {}\tProtocol: {}".format(result_config['routingStrategy'], result_config['mode'], result_config['protocol'])
+                    print("\tRouting Strategy: {}\tMode: {}\tProtocol: {}".format(result_config['routingStrategy'], result_config['mode'], result_config['protocol']))
                     result_status = self.__router_call("/routes/%s/status" % route_name)
                     result_status_json= json.loads(result_status.content)
                     print("\tTotal Connections: %d\tActive Connections: %d\tBlocked Hosts: %d"
