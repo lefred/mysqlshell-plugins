a44e33269e8613210ec6e835deef2b15bdd8358d
Frederic Descamps <lefred.descamps@gmail.com>
2021-03-03 10:57:33 +0100

    Query Execution Plan save and compare
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

---
+++ qep/common.py
@@ -0,0 +1,174 @@
+import mysqlsh
+import json
+from mysqlsh.plugin_manager import plugin, plugin_function
+from mysqlsh_plugins_common import run_and_show
+
+def _is_qep_db_existing(session):
+   stmt = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='dba'"
+   result = session.run_sql(stmt)
+   row = result.fetch_all()
+   if len(row) > 0:
+       return True
+   return False
+
+def _is_qep_table_existing(session):
+   stmt = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='dba' AND TABLE_NAME='qep'"
+   result = session.run_sql(stmt)
+   row = result.fetch_all()
+   if len(row) > 0:
+       return True
+   return False
+
+
+def _chek_dba_db(session, dbok):
+    shell = mysqlsh.globals.shell
+    # check if the database 'dba' exists
+    create_table_stmt = """CREATE TABLE dba.qep (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `common_hash` varchar(56) DEFAULT NULL,
+  `inserted` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `qep` json DEFAULT NULL,
+  `query_cost` float GENERATED ALWAYS AS (json_unquote(json_extract(`qep`,_utf8mb4'$."query_bloack"."cost_info"."query_cost"'))) VIRTUAL,
+  `qep_tree` text,
+  PRIMARY KEY (`id`),
+  KEY `hash_idx` (`common_hash`)
+) ENGINE=InnoDB"""
+    if not dbok:
+        if _is_qep_db_existing(session):
+            if _is_qep_table_existing(session):
+                dbok = True
+            else:
+                answer = shell.prompt("The table 'qep' is missing in schema 'dba', do you wanna create it? (y/N) ", {'defaultValue':'n'})
+                if answer.lower() == 'y':
+                    try:
+                        session.run_sql(create_table_stmt)
+                    except:
+                        print("ERROR: problem creating 'qep' table.")
+                        return dbok
+        else:
+            answer = shell.prompt("The schema 'dba' is missing, do you wanna create it? (y/N) ", {'defaultValue':'n'})
+            if answer.lower() == 'y':
+               query = """CREATE DATABASE dba"""
+               try:
+                    session.run_sql(query)
+                    try:
+                        session.run_sql(create_table_stmt)
+                    except:
+                        print("ERROR: problem creating 'qep' table.")
+                        return dbok
+               except:
+                   print("ERROR: problem creating 'dba' schema.")
+                   return dbok
+
+    return True
+
+
+def _save_qep(stmt, qep, qep_tree, session, dbok):
+    dbok = _chek_dba_db(session, dbok)
+    # save the json qep
+    query = """INSERT INTO dba.qep(common_hash, qep, qep_tree)
+               VALUES (SHA2(TRIM(statement_digest_text(?)), 224),
+                ?, ?);"""
+    session.run_sql(query, [stmt, qep, qep_tree])
+    return dbok
+
+def _load_qep(stmt, qep, session, dbok):
+    dbok = _chek_dba_db(session, dbok)
+
+    query = """SELECT inserted, query_cost FROM dba.qep WHERE common_hash=SHA2(TRIM(statement_digest_text(?)), 224)
+               ORDER BY inserted DESC LIMIT 1"""
+    result = session.run_sql(query, [stmt])
+    row = result.fetch_one()
+    got_one = False
+    if row:
+        print("The last QEP saved for this query on {} had a cost of {}".format(row[0], row[1]))
+        got_one = True
+    return dbok, got_one
+
+def _load_qep_all(stmt, session):
+    query = """SELECT id, inserted, query_cost FROM dba.qep WHERE common_hash=SHA2(TRIM(statement_digest_text(?)), 224)
+               ORDER BY inserted DESC"""
+    result = session.run_sql(query, [stmt])
+    rows = result.fetch_all()
+    ids=[]
+    for row in rows:
+        rec=[]
+        rec.append(row[0])
+        rec.append(row[1])
+        rec.append(row[2])
+        ids.append(rec)
+
+    return ids
+
+def _get_qep_by_id(id, session):
+    query = """SELECT qep, qep_tree FROM dba.qep WHERE id=?"""
+    result = session.run_sql(query, [id])
+    row = result.fetch_one()
+    return(row[0], row[1])
+
+def get_full_detail(original_query, session, dbok):
+    shell = mysqlsh.globals.shell
+    stmt = """EXPLAIN FORMAT=json %s""" % original_query
+    result = session.run_sql(stmt)
+    row = result.fetch_one()
+    qep=row[0]
+    qep_json = json.loads(qep)
+    print("The cost of the query is {}".format(qep_json["query_block"]["cost_info"]["query_cost"]))
+    answer = shell.prompt('Do you want to have EXPLAIN output? (y/N) ', {'defaultValue':'n'})
+    if answer.lower() == 'y':
+        stmt = """EXPLAIN %s""" % original_query
+        run_and_show(stmt,'vertical')
+    answer = shell.prompt('Do you want to have EXPLAIN in JSON format output? (y/N) ', {'defaultValue':'n'})
+    if answer.lower() == 'y':
+        print(qep)
+    stmt = """EXPLAIN format=tree %s""" % original_query
+    result = session.run_sql(stmt)
+    row = result.fetch_one()
+    qep_tree=row[0]
+    answer = shell.prompt('Do you want to have EXPLAIN in TREE format output? (y/N) ', {'defaultValue':'n'})
+    if answer.lower() == 'y':
+        print(qep_tree)
+    answer = shell.prompt('Do you want to have EXPLAIN ANALYZE output? (y/N) ', {'defaultValue':'n'})
+    if answer.lower() == 'y':
+        stmt = """EXPLAIN ANALYZE %s""" % original_query
+        result = session.run_sql(stmt)
+        row = result.fetch_one()
+        print(row[0])
+
+    dbok, got_one = _load_qep(original_query, qep, session, dbok)
+
+    if dbok and got_one:
+        answer = shell.prompt('Do you want to compare with a previous QEP? (y/N) ', {'defaultValue':'n'})
+        if answer.lower() == 'y':
+            all_ids = _load_qep_all(original_query,session)
+            i = 0
+            if len(all_ids)>0:
+                fmt = "| {0:>3s} | {1:20s} | {2:>11s} |"
+                header = fmt.format("Num", "Timestamp", "Query Cost")
+                bar = "+" + "-" * 5 + "+" + "-" * 22 + "+" + "-" * 13 + "+"
+                print (bar)
+                print (header)
+                print (bar)
+                for rec in all_ids:
+                    i+=1
+                    print(fmt.format(str(i), str(rec[1]), str(rec[2])))
+                print (bar)
+
+                while True:
+                    answer = shell.prompt('With which previous QEP do you want to compare? (1) ', {'defaultValue':'1'})
+                    if answer.isdigit():
+                        if int(answer) >0 and int(answer) <= len(all_ids):
+                            qep2, qep2_tree = _get_qep_by_id(all_ids[int(answer)-1][0], session)
+                            print("\033[33mCURRENT:\n--------\n{}\033[0m".format(qep_tree))
+                            print("\033[36mPREVIOUS:\n---------\n{}\033[0m".format(qep2_tree))
+                            print()
+                            break
+
+
+    answer = shell.prompt('Do you want to save the QEP? (y/N) ', {'defaultValue':'n'})
+    if answer.lower() == 'y':
+        _save_qep(original_query, qep, qep_tree, session, dbok)
+
+
+
+    return dbok
---
+++ qep/init.py
@@ -0,0 +1,68 @@
+from mysqlsh.plugin_manager import plugin, plugin_function
+from mysqlsh_plugins_common import get_version
+from qep import common
+
+global dbok
+
+dbok=False
+
+@plugin
+class qep:
+    """
+    Query Execution Plan utilities.
+
+    A collection of tools and utilities to perform checks on your
+    MySQL Query Execution Plan
+    """
+
+@plugin_function("qep.get")
+def get_qep(session=None):
+    """
+    Prints the Query Execution Plan for a query"
+
+    This function prints the QEP for a query in different format:
+       - traditional
+       - tree
+       - json
+    It will also run EXPLAIN ANALYZE and compare with existing
+    saved QEP for the query if any.
+    It will also propose to save the QEP in a dedicate schema and
+    table: dba.qep
+
+    Args:
+        session (object): The optional session object used to query the
+            database. If omitted the MySQL Shell's current session will be used.
+
+    """
+    import mysqlsh
+    shell = mysqlsh.globals.shell
+
+    global dbok
+
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                  "function or connect the shell to a database")
+            return
+    if shell.parse_uri(session.get_uri())['scheme'] != "mysqlx":
+        print("For more details, please use a MySQL X connection.")
+        return
+    if session.get_current_schema() is None:
+        print("Warning: no schema has been chosen, your query needs to specify all used scheme.")
+    stmt = ""
+    statement_enter = shell.prompt("Enter the query (end it with ';'): ")
+    while True:
+        stmt += statement_enter
+        if ";" in statement_enter:
+            if statement_enter.index(";") == len(statement_enter.rstrip())-1:
+                break
+            else:
+                stmt += ' '
+                statement_enter = shell.prompt("> ")
+        else:
+            stmt += ' '
+            statement_enter = shell.prompt("> ")
+    #print(stmt)
+    dbok = common.get_full_detail(stmt, session, dbok)
+
