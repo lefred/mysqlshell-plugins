1ff07ba121abe2dc3448c7b383a04315e981815d
Frederic Descamps <lefred.descamps@gmail.com>
2023-09-22 09:48:55 +0200

    update the oci MySQL HeatWave Database Service allowed privileges
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- user/mds.py
+++ user/mds.py
@@ -1,5 +1,6 @@
 
-
+# see  mysql-shell/modules/util/dump/compatibility.cc
+#      https://docs.oracle.com/en-us/iaas/mysql-database/doc/mysql-server.html#GUID-410B2C03-5238-463E-85FA-7E9861ABA0C0#MYAAS-GUID-410B2C03-5238-463E-85FA-7E9861ABA0C0
 mds_allowed_privileges = [
     "USAGE",
     "ALTER",
@@ -29,9 +30,15 @@ mds_allowed_privileges = [
     "TRIGGER",
     "UPDATE",
     "APPLICATION_PASSWORD_ADMIN",
+    "AUDIT_ADMIN",
+    "FLUSH TABLES",
     "CONNECTION_ADMIN",
+    "FLUSH_OPTIMIZER_COSTS",
+    "FLUSH_USER_RESOURCES",
+    "FLUSH_STATUS",
     "RESOURCE_GROUP_ADMIN",
     "RESOURCE_GROUP_USER",
+    "ROLE_ADMN",
     "XA_RECOVER_ADMIN"
 ]
 
